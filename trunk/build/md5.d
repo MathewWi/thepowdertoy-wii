e11624322b1e3744a046585467cb40acc05b8cad
castlevania7689@yahoo.com <castlevania7689@yahoo.com@1182a322-7c46-10d8-1a36-fb768a2f640f>
2010-08-12 19:02:09 +0000

    Removing unnecessary files

--- trunk/build/md5.d
+++
@@ -1,4 +0,0 @@
-md5.o: c:/projects/wii/FacialTurd-The-Powder-Toy-1319e92/source/md5.c \
- c:/projects/wii/FacialTurd-The-Powder-Toy-1319e92/source/md5.h
-
-c:/projects/wii/FacialTurd-The-Powder-Toy-1319e92/source/md5.h:
--- trunk/build/update.d
+++
@@ -1,5 +0,0 @@
-update.o: \
- c:/projects/wii/FacialTurd-The-Powder-Toy-1319e92/source/update.c \
- c:/projects/wii/FacialTurd-The-Powder-Toy-1319e92/source/update.h
-
-c:/projects/wii/FacialTurd-The-Powder-Toy-1319e92/source/update.h:
